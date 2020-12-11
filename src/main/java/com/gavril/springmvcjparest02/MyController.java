7cd21344d7112587b7bcc4c5b7c641740200ea8a
gavril.hasmasan <gavril.hasmasan@pitechnologies.ro>
2020-12-11 16:25:22 +0200

    testing postman

--- src/main/java/com/gavril/springmvcjparest02/MyController.java
+++ src/main/java/com/gavril/springmvcjparest02/MyController.java
@@ -3,12 +3,15 @@ package com.gavril.springmvcjparest02;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.PostLoad;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -53,13 +56,15 @@ public class MyController {
 		return "hi";
 	}
 	
-	@GetMapping("getPersonByName")
-	public List<Person> getPersonByName(@RequestParam("name") String name1, Model m) {
-		
-		return myrepo.getPersonByName(name1);
+	@PostMapping("getPersonByName")
+	@ResponseBody
+	public List<Person> getPersonByName(@RequestBody Person person, Model m) {
+			String name = person.getName();
+					System.out.println(name);
+		return myrepo.getPersonByName(name);
 	}
 	
-	@GetMapping("getPersonByName/{name}")
+	@GetMapping(value = "getPersonByName/{name}")
 	public List<Person> getPersonByNamePathVariable(@PathVariable("name") String name1, Model m) {
 		
 		return myrepo.getPersonByName(name1);
@@ -72,7 +77,8 @@ public class MyController {
 		return "show";
 	}
 	
-	@RequestMapping(value = "person/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "person/{id}", method = RequestMethod.POST)
+	@ResponseBody
 	public Optional<Person> getPerson(@PathVariable("id") int id) {
 //		ModelAndView mv = new ModelAndView("show");
 ////		mv.setViewName("show");
