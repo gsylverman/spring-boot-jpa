9f4e645cd9d1a0b2b1729a65058ddb4573a717c2
gavril.hasmasan <gavril.hasmasan@pitechnologies.ro>
2020-12-11 12:02:48 +0200

    aas

--- src/main/java/com/gavril/springmvcjparest02/MyController.java
+++ src/main/java/com/gavril/springmvcjparest02/MyController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.gavril.springmvcjparest02.dao.MyRepoI;
@@ -35,7 +37,20 @@ public class MyController {
 		return "show";
 	}
 	
-	@RequestMapping(value = "getPerson", method = RequestMethod.GET)
+	@GetMapping("getPeople")
+	public String getPeopleList(Model m) {
+		m.addAttribute("people", myrepo.findAll());
+		return "show";
+	}
+	
+	@GetMapping("getPersonByName")
+	public String getPersonByName(@RequestParam("name") String name1, Model m) {
+		
+		m.addAttribute("people", myrepo.getPersonByName(name1));
+		
+		return "show";
+	}
+	@RequestMapping(value = "getPersonById", method = RequestMethod.GET)
 	public ModelAndView getPerson(@RequestParam("id") int id) {
 		ModelAndView mv = new ModelAndView("show");
 //		mv.setViewName("show");
--- src/main/java/com/gavril/springmvcjparest02/dao/MyRepoI.java
+++ src/main/java/com/gavril/springmvcjparest02/dao/MyRepoI.java
@@ -1,10 +1,14 @@
 package com.gavril.springmvcjparest02.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gavril.springmvcjparest02.model.Person;
 
 public interface MyRepoI extends JpaRepository<Person, Integer> {
+
+	List<Person> getPersonByName(String name);
 
 	
 }
